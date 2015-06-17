ae231e4c8653db28a6a6dbabf36d40524aeb0604
Jovia Nierenberg <nierenberg.jovia@gmail.com>
2015-06-17 15:05:20 -0400

    rendering the template using the swig templating engine section complete.

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require( 'express' );
 var morgan = require('morgan');
+var swig = require('swig');
 var app = express();
 app.use(morgan('dev'));
 
+// swig
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
+
 app.get('/', function (req, res) {
-  res.send('hello, world and friends!')
+  //res.send('hello, world and friends!')
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
 var server = app.listen(3000, function () {
@@ -16,4 +26,3 @@ var server = app.listen(3000, function () {
 
 });
 
-// added a comment
